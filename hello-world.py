a04481cc7f1defe122bdc1b4da141e2d1d3b5d78
Melvin malaya <malayamelvin@gmail.com>
2022-08-04 13:51:57 +0200

    Update hello-world.py
    Melvins commit

--- hello-world.py
+++ hello-world.py
@@ -2,3 +2,6 @@ print("My name is Vikwato Kamanga.\n"
       "I am currently pursuing a Masters of Science Degree in Analytics from Georgia Insititute of Technology\n"
       "I enjoy programming, data management and system administration\n"
       "Outside work, I enjoy watching and playing sport, listening to music, playing video games and watching TV.")
+
+
+print("Hello my name is Melvin Malaya, I enjoy learning about new technologies and coding in Java")
